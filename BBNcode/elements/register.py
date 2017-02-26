814bfde8ecb107a870edfced1cfe94b2dc60a414
Anton Lioznov <Lavton@gmail.com>
2017-02-26 11:11:34 +0300

    prob add jacob

--- BBNcode/elements/register.py
+++ BBNcode/elements/register.py
@@ -19,6 +19,7 @@ class Registrator():
     def __init__(self):
         self.X_0 = []
         self.elements = []
+        self.rev_element_list = {}
         self.ode_form = []
 
 
@@ -27,37 +28,50 @@ class Registrator():
         # for i in range(len(self.elements)):
             # self.ode_form[i] 
         self.elements.append(element)
+        self.rev_element_list[element.str_view] = len(self.elements) - 1
 
-    # def finish_registration(self):
-        # for i in range(len(self.elements)):
-            # element = self.elements[i]
+    def finish_registration(self):
+        self.ode_funcs = []
+        for i in range(len(self.elements)):
+            element = self.elements[i]
+            # ode_func = []
+            self.ode_funcs.append([])
+            for key, value in element.ode_elem.items():
+                self.ode_funcs[self.rev_element_list[key]].append(value)
             # self.X_0.append(element.X_0)
+        for i in range(len(self.ode_funcs)):
+            self.ode_funcs[i] = lambda X, T: sum(map(lambda f: f(X, T), self.ode_funcs[i]))
 
-            # def o_form(X, T):
-                # dX = defaultdict(lambda : 0)
-                # for j in range(i):
-                    # dX[element.str_view] += element.ode_elem(X, T).get(element.str_view, 0)
-                # return dX
-            # self.ode_form.append(o_form)
+        self.jacob_funcs = [[[lambda X, T: 0] for _ in self.elements] for _ in self.elements]
+        for i in range(len(self.elements)):
+            element = self.elements[i]
+            for key, value in element.jacob.items():
+                for k, v in value.items():
+                    self.jacob_funcs[self.rev_element_list[key]][self.rev_element_list[k]].append(v)
+        for i in range(len(self.jacob_funcs)):
+            for j in range(len(self.jacob_funcs[i])):
+                self.jacob_funcs[i][j] = lambda X, T: sum(map(lambda f: f(X, T), self.jacob_funcs[i][j]))
 
     def ode_int(X, T):
-        dX = defaultdict(lambda: 0)
-        X_new = defaultdict(lambda: 0)
-        for i in range(len(self.elements)):
-            X_new[self.elements[i].str_view] = X[i]
+
+        # dX = defaultdict(lambda: 0)
+        # X_new = defaultdict(lambda: 0)
+        dX = []
+        X_new = []
+        # for i in range(len(self.elements)):
+            # X_new.a = X[i]
         for element in self.elements:
             for equ in element.ode_elem(X, T)
                 dX[element.str_view] += equ
         f_dX = []
         for i in range(len(self.elements)):
             f_dX.append(dX[self.elements[i]])
-
         return f_dX
 
 registrator = Registrator()
 registrator.registrate(el.n)
 registrator.registrate(el.H_1)
 
-# registrator.finish_registration()
+registrator.finish_registration()
 
 X_0 = registrator.X_0
