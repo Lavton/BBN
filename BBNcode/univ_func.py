76a6749dae06374e96d75a8f4f5cc0ddafe865da
Anton Lioznov <Lavton@gmail.com>
2017-06-01 11:29:26 +0300

    del commented lines

--- BBNcode/univ_func.py
+++ BBNcode/univ_func.py
@@ -9,7 +9,6 @@ def __photon_density__(T):
     """
     # T in eV
     zeta_3 = 1.202
-    # return (2 * (8*pi)/c**3 * zeta_3) / (h/(2*pi*constants.to_norm_tempreture(T)))**3
     return 0.244*(constants.to_norm_tempreture(T)/(h*c))**3
 
 def __proton_mass_density__(T):
@@ -17,5 +16,4 @@ def __proton_mass_density__(T):
     return (m_p/5.60958835719e+32) * __photon_density__(T) * nu_n
 
 def rat_scale(T):
-    return __proton_mass_density__(T)   # * constants.N_a
-    # return __photon_density__(T) * nu_n * constants.N_a
+    return __proton_mass_density__(T)  
